b31fe168aac7d82cae0a6a2108104bff8fc53661
Kradon Zhao <kradonzhao@gmail.com>
2020-05-19 18:58:04 -0500

    implement basic functions

--- src/zTest/ZScore.java
+++ src/zTest/ZScore.java
@@ -1,5 +1,21 @@
 package zTest;
 
 public interface ZScore {
-
+	boolean normality();
+	/*
+	 * Checks to see if the given sample comes from a random distribution through the Central Limit Theorem. 
+	 * If it is passes the conditions for the theorem, it will return true, otherwise false.
+	 */
+	double score();
+	/*
+	 * Returns the z score that is calculated by the program
+	 */
+	double sd();
+	/*
+	 * Calculates the Standard Deviation for the given sample.
+	 */
+	int prob(int score);
+	/*
+	 * converts the z-score into probability of happening. 
+	 */
 }
